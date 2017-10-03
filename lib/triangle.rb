070b47f947d4516202a13a5d16c52f9c1debf2ee
Purnima  Lakshmi Narayanan <purnima0912@hotmail.com>
2017-10-03 13:17:26 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,9 +6,21 @@ class Triangle
     @lengthB = lengthB
     @lengthC = lengthC
   end
-
-
   def kind
+    if (@lengthA <= 0 || @lengthB <= 0 || @lengthC <= 0 || (@lengthA + @lengthB) <= @lengthC || (@lengthB + @lengthC) <= @lengthA || (@lengthA + @lengthC) <= @lengthB )
+      # begin
+      raise TriangleError
+      # rescue TriangleError
+          # puts error.message
+      # end
+    elsif(@sideA == @sideB && @sideB ==@sideC && sideA > 0)
+      return :equilateral
+    elsif (@sideA == @sideB || @sideA == @sideC || @sideB == @sideC)
+      return :isosceles
+    elsif (@sideA != @sideB && @sideB!=@sideC)
+      return :scalene
+    end
+  end
 
   end
 end
